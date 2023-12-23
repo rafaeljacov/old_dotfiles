61748a7dc61b2fa14572ed19daef184024005455
Rafael Jacov Medel <medelrafjac@gmail.com>
2023-12-23 12:09:21 +0800

    update config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -37,7 +37,7 @@ return {
             require('gitsigns').setup()
         end
     },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
