ae2cabeed28da7ca14ab94391a8b48c6e44d5679
Rafael Jacov Medel <medelrafjac@gmail.com>
2023-12-24 15:34:44 +0800

    remove unecessary line

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -114,8 +114,5 @@ return {
         'barrett-ruth/live-server.nvim',
         build = "npm i -g live-server",
         config = true,
-        init = function()
-            require('live-server').setup()
-        end
     },
 }
