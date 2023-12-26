040001201225f325b206700447a9a6b813558e83
Rafael Jacov Medel <medelrafjac@gmail.com>
2023-12-26 12:43:03 +0800

    refactored repeating keymaps as loop

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -8,15 +8,9 @@ vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc
 vim.keymap.set("n", "<leader>hp", function() harpoon:list():prepend() end, { desc = "Harpoon Prepend" })
 vim.keymap.set("n", "<leader>H", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Menu" })
 
-vim.keymap.set("n", "1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "4", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "5", function() harpoon:list():select(5) end)
-vim.keymap.set("n", "6", function() harpoon:list():select(6) end)
-vim.keymap.set("n", "7", function() harpoon:list():select(7) end)
-vim.keymap.set("n", "8", function() harpoon:list():select(8) end)
-vim.keymap.set("n", "9", function() harpoon:list():select(9) end)
+for i = 1, 9 do
+    vim.keymap.set("n", string.format("<leader>h%d",i), function() harpoon:list():select(i) end, { desc = "select" })
+end
 
 -- Toggle previous & next buffers stored within Harpoon list
 -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
