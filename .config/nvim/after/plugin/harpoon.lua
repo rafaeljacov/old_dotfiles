40d166fe5c5d21d60f91a460414acd7f3f16144e
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-01-19 13:34:38 +0800

    change to <leader>hl toggle harpoon list

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -6,7 +6,7 @@ harpoon:setup()
 
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = "Harpoon Append" })
 vim.keymap.set("n", "<leader>hp", function() harpoon:list():prepend() end, { desc = "Harpoon Prepend" })
-vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon Menu" })
+vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon List" })
 
 for i = 1, 9 do
     vim.keymap.set("n", string.format("<leader>h%d",i), function() harpoon:list():select(i) end, { desc = "select" })
