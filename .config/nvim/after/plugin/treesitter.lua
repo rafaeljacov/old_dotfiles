3050268c12b318225396ba0b04d271f0ca990ebd
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-01-28 15:13:01 +0800

    adjust spacings

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,29 +1,29 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query",
-  'astro', 'css', 'glimmer', 'graphql', 'html', 'javascript',
-  'nix', 'php', 'python', 'scss', 'svelte', 'tsx', 'twig',
-  'typescript',  'vue', 'markdown', 'markdown_inline', 'sql'},
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query",
+        'astro', 'css', 'glimmer', 'graphql', 'html', 'javascript',
+        'nix', 'php', 'python', 'scss', 'svelte', 'tsx', 'twig',
+        'typescript', 'vue', 'markdown', 'markdown_inline', 'sql' },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
--- List of parsers to ignore installing (for "all")
-  ignore_install = { "latex" },
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = { "latex" },
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
- autotag = {
-    enable = true,
-  },
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+    autotag = {
+        enable = true,
+    },
 }
