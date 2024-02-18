9cfa3a4cce2160612042c6a2f5c9b7ec704b0125
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-02-18 19:49:23 +0800

    set cmp borders

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,39 +1,38 @@
 local lsp_zero = require('lsp-zero')
-local lspconfig = require('lspconfig')
 
 lsp_zero.on_attach(function(client, bufnr)
--- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-  lsp_zero.highlight_symbol(client, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
+    lsp_zero.highlight_symbol(client, bufnr)
 end)
 
 vim.keymap.set('n', '<leader><CR>', '<Cmd>LspZeroFormat<cr>')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-    'cssls',
-    'tsserver',
-    'emmet_ls',
-    'html',
-    'eslint',
-    'clangd',
-    'rust_analyzer',
-    'texlab',
-    'gopls',
-    'pyright',
-    'bashls',
-    'lua_ls',
-    'marksman'
-},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+    ensure_installed = {
+        'cssls',
+        'tsserver',
+        'emmet_ls',
+        'html',
+        'eslint',
+        'clangd',
+        'rust_analyzer',
+        'texlab',
+        'gopls',
+        'pyright',
+        'bashls',
+        'lua_ls',
+        'marksman'
+    },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    }
 })
 
 local cmp = require('cmp')
@@ -55,9 +54,13 @@ cmp.setup({
     completion = {
         completeopt = 'menu,menuone,noinsert'
     },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
     mapping = cmp.mapping.preset.insert({
         -- confirm completion
-        ['<C-y>'] = cmp.mapping.confirm({select = true}),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 
         -- scroll up and down the documentation window
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
@@ -76,8 +79,14 @@ cmp.setup({
     }
 })
 
+local lspconfig = require('lspconfig')
+local util = require('lspconfig/util')
+
+-- Go
 lspconfig.gopls.setup {
-    cmd = {'gopls'},
+    cmd = { 'gopls' },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_dir = util.root_pattern('go.work', 'go.mod', '.git'),
     settings = {
         gopls = {
             usePlaceholders = true,
