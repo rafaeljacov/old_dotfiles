ace6209ed469c42e9cdf81e7dd3854b8f0912f1e
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-03-28 12:23:14 +0800

    Revert "updated workspace binds"
    This reverts commit 81046ed514e902251c7164f444cbefeb0ba452e4.

--- .config/hypr/scripts/workspace.py
+++ .config/hypr/scripts/workspace.py
@@ -4,22 +4,8 @@ import sys
 import subprocess
 
 def switch_ws(workspace):
-    # To be implemented
-    if workspace[0] == 'r':
-        if workspace[1] == '+':
-            pass
-        elif workspace[1] == '-':
-            pass
-
     subprocess.run(['hyprctl', 'dispatch', 'workspace', workspace])
     subprocess.run(['eww', 'update', f'active-ws={workspace}'])
 
-def moveto_ws(workspace):
-    subprocess.run(['hyprctl', 'dispatch', 'movetoworkspace', workspace])
-    subprocess.run(['eww', 'update', f'active-ws={workspace}'])
-
-
 if sys.argv[1] == '-s':
     switch_ws(sys.argv[2])
-elif sys.argv[1] == '-m':
-    moveto_ws(sys.argv[2])
