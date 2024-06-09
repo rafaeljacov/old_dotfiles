b420ab37322efe5ffb1a2391ccb4541b57644dc2
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-06-09 21:16:32 +0800

    change workspaces to permanent

--- .config/ags/bar.js
+++ .config/ags/bar.js
@@ -26,12 +26,13 @@ export default function Bar(monitor = 0) {
 
 function Workspaces() {
     const activeId = hyprland.active.workspace.bind("id")
-    const workspaces = hyprland.bind("workspaces")
-        .as(ws => ws.filter(({ id }) => id > 0).map(({ id }) => Widget.Button({
+    const workspaces = [1, 2, 3, 4, 5, 6].map(id =>
+        Widget.Button({
             on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
             child: Widget.Label(`${id}`),
             class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
-        })))
+        })
+    )
 
     return Widget.Box({
         class_name: "workspaces",
