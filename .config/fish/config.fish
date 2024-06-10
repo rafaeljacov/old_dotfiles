4901798efd263e5755937e5d86beab3500a18e6a
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-06-10 16:31:30 +0800

    remove right of transient prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,10 +34,6 @@ function starship_transient_prompt_func
   starship module character
 end
 
-function starship_transient_rprompt_func
-  starship module time
-end
-
 # Enable node & npm while ignoring the stdout
 nvm use latest > /dev/null
 
