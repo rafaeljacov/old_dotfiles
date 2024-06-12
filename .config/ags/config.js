de4d91ba9fb6dd35c2cff3f2da772dc5ef5f063f
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-06-12 08:26:09 +0800

    refactor: move widgets and styles to their own directory

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,8 +1,8 @@
-import Bar from './bar.js'
-import NotificationPopups from './notifications.js'
+import Bar from './widgets/bar.js'
+import NotificationPopups from './widgets/notifications.js'
 
 App.config({
-    style: "./style.css",
+    style: "./style/style.css",
     windows: [
         Bar(0),
         NotificationPopups()
