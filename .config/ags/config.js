f456efc037defd54d67b8c5fba1be0975fe08778
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-06-12 09:08:37 +0800

    add: auto reload config functions

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,8 +1,38 @@
 import Bar from './widgets/bar.js'
 import NotificationPopups from './widgets/notifications.js'
 
+const style_dir = `${App.configDir}/style`
+const widgets_dir = `${App.configDir}/widgets`
+
+// Reload ags when modifying config and widgets
+function reloadAgs(path) {
+    Utils.monitorFile(
+        path,
+
+        function() {
+            Utils.execAsync('bash -c "ags quit && ags"')
+        }
+    )
+}
+
+reloadAgs(widgets_dir)
+reloadAgs(`${App.configDir}/config.js`)
+
+// Auto reload styles
+Utils.monitorFile(
+    style_dir,
+
+    function() {
+        // target css file
+        const css = `${style_dir}/style.css`
+
+        App.resetCss()
+        App.applyCss(css)
+    },
+)
+
 App.config({
-    style: "./style/style.css",
+    style: `${style_dir}/style.css`,
     windows: [
         Bar(0),
         NotificationPopups()
