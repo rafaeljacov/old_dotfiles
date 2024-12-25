8d1fbb6c8e237ddfa7a09e04a3cb039e46313758
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-12-25 16:32:04 +0800

    add plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,16 +1,9 @@
 return {
-    { "folke/neodev.nvim",    opts = {} },
 
     'onsails/lspkind.nvim',
     'tpope/vim-fugitive',
     'nanotee/zoxide.vim',
-
-    -- Debugger
-    { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
-    'jay-babu/mason-nvim-dap.nvim',
-    'theHamsta/nvim-dap-virtual-text',
-
-    { 'lewis6991/gitsigns.nvim', opts = {} },
-    {'numToStr/Comment.nvim', opts = {}},
     'windwp/nvim-ts-autotag',
+    { 'lewis6991/gitsigns.nvim', opts = {} },
+    { 'numToStr/Comment.nvim',   opts = {} },
 }
