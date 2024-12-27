58fdc63c1defe50563426056bea01c386c1834bb
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-12-28 06:58:53 +0800

    remove codelldb

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -3,22 +3,6 @@ return {
     version = '^5', -- Recommended
     lazy = false,   -- This plugin is already lazy
     config = function()
-        -- Update this path
-        local extension_path = vim.env.HOME .. '/.local/share/nvim/mason/packages/codelldb/extension/'
-        local codelldb_path = extension_path .. 'adapter/codelldb'
-        local liblldb_path = extension_path .. 'lldb/lib/liblldb'
-        local this_os = vim.uv.os_uname().sysname;
-
-        -- The path is different on Windows
-        if this_os:find "Windows" then
-            codelldb_path = extension_path .. "adapter\\codelldb.exe"
-            liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-        else
-            -- The liblldb extension is .so for Linux and .dylib for MacOS
-            liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-        end
-
-        local cfg = require('rustaceanvim.config')
         vim.g.rustaceanvim = {
             -- Plugin configuration
             tools = {
@@ -59,7 +43,6 @@ return {
             },
             -- DAP configuration
             dap = {
-                adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
             },
         }
     end
