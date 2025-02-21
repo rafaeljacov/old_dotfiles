1ec0df8311ee3bf58ccefad0b371dddd571fc4cb
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-02-21 20:30:49 +0800

    add: inline diagnostics plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -7,4 +7,13 @@ return {
     'Yazeed1s/oh-lucy.nvim',
     { 'lewis6991/gitsigns.nvim', opts = {} },
     { 'numToStr/Comment.nvim',   opts = {} },
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy", -- Or `LspAttach`
+        priority = 1000, -- needs to be loaded in first
+        config = function()
+            require('tiny-inline-diagnostic').setup()
+            vim.diagnostic.config({ virtual_text = false })
+        end
+    }
 }
