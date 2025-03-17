697a5b0888ac497ae4e651ede6bdba4c652b4310
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-17 14:18:57 +0800

    add ags_bin target

--- .config/ags/flake.nix
+++ .config/ags/flake.nix
@@ -1,14 +1,36 @@
 {
+  description = "AGS Shell";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     ags.url = "github:aylur/ags";
   };
 
-  outputs = { self, nixpkgs, ags }: let
+  outputs = {
+    self,
+    nixpkgs,
+    ags,
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    astallibs = with ags.packages.${system}; [
+      io
+      hyprland
+      battery
+      mpris
+      apps
+      wireplumber
+      tray
+      notifd
+      network
+      bluetooth
+    ];
+    ags_package = (
+      ags.packages.${system}.ags.override {
+        extraPackages = astallibs;
+      }
+    );
   in {
-    packages.${system}.default = ags.lib.bundle { 
+    packages.${system}.default = ags.lib.bundle {
       inherit pkgs;
       src = ./.;
       name = "ags-shell"; # name of executable
@@ -16,18 +38,9 @@
       gtk4 = false;
 
       # additional libraries and executables to add to gjs' runtime
-      extraPackages = with ags.packages.${system}; [
-        io
-        hyprland
-        battery
-        mpris
-        apps
-        wireplumber
-        tray
-        notifd
-        network
-        bluetooth
-      ];
+      extraPackages = astallibs;
     };
+
+    ags_bin = ags_package;
   };
 }
