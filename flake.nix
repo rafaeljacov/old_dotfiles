7e8245af61d357475d1b15f0cbd523b0f6cfa8c8
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-25 07:58:45 +0800

    switch to flakes

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    zen-browser,
+    ...
+  }: let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {inherit inputs;};
+
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.rafaeljacov = import ./home.nix;
+          home-manager.backupFileExtension = "backup";
+
+          # use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+          home-manager.extraSpecialArgs = {
+            inherit inputs system;
+          };
+        }
+      ];
+    };
+  };
+}
