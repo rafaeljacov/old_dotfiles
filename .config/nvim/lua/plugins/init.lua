777c9f01b7ba1ddd31d52a2a91c9cf5c6328998c
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-25 23:36:15 +0800

    remove oh-lucy colorscheme plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,7 +4,6 @@ return {
     'tpope/vim-fugitive',
     'nanotee/zoxide.vim',
     'windwp/nvim-ts-autotag',
-    'Yazeed1s/oh-lucy.nvim',
     { 'lewis6991/gitsigns.nvim', opts = {} },
     { 'numToStr/Comment.nvim',   opts = {} },
     {
