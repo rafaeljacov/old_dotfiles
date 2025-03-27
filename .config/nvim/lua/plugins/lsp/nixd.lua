a7af92c71f43114ff05749695f765e61f2200db7
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-27 16:42:15 +0800

    comment out home-manager

--- .config/nvim/lua/plugins/lsp/nixd.lua
+++ .config/nvim/lua/plugins/lsp/nixd.lua
@@ -13,10 +13,10 @@ return {
                     expr =
                     '(builtins.getFlake ("/home/rafaeljacov/dotfiles")).nixosConfigurations.huawei-nixos.options',
                 },
-                home_manager = {
-                    expr =
-                    '(builtins.getFlake ("/home/rafaeljacov/dotfiles")).homeConfigurations."rafaeljacov@huawei-nixos".options',
-                },
+                -- home_manager = {
+                --     expr =
+                --     '(builtins.getFlake ("/home/rafaeljacov/dotfiles")).homeConfigurations."rafaeljacov@huawei-nixos".options',
+                -- },
             },
         }
     }
